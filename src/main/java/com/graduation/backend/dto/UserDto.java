5581ad764d68c0fb0f4b50fed14e30920d217a01
SeungHwan Oh <wxtq22@naver.com>
2025-03-28 19:09:46 +0900

    .

---
+++ src/main/java/com/graduation/backend/dto/UserDto.java
@@ -0,0 +1,12 @@
+package com.graduation.backend.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDto {
+    private String email;
+    private String nickname;
+}
