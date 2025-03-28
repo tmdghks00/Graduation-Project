6c6b81a368ffdfc8e1e671fcaf8a66da33ba7eeb
SeungHwan Oh <wxtq22@naver.com>
2025-03-28 18:36:05 +0900

    .

---
+++ src/main/java/com/graduation/backend/controller/UserController.java
@@ -0,0 +1,36 @@
+package com.graduation.backend.controller;
+
+import com.graduation.backend.dto.*;
+import com.graduation.backend.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService service) {
+        this.userService = service;
+    }
+
+    @PostMapping("/signup")
+    public void signup(@RequestBody UserSignupRequest request) {
+        userService.signup(request);
+    }
+
+    @GetMapping("/{id}")
+    public UserDto getUser(@PathVariable Long id) {
+        return userService.getUser(id);
+    }
+
+    @PutMapping("/{id}")
+    public void updateNickname(@PathVariable Long id, @RequestParam String nickname) {
+        userService.updateNickname(id, nickname);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+    }
+}
