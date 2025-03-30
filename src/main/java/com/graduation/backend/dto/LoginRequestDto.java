69bf5d1d111b49b4f99bdb54b7880d4c6d5c94bf
seunghwan <wxtq22@naver.com>
2025-03-30 20:58:27 +0900

    .

--- src/main/java/com/graduation/backend/dto/LoginRequestDto.java
+++ src/main/java/com/graduation/backend/dto/LoginRequestDto.java
@@ -6,6 +6,6 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class LoginRequestDto {
-    private String email;
-    private String password;
+    private String email; // 이메일
+    private String password; // 비밀번호
 }
